dcf07c41495eb1baeb0e965cbb56df4b1ad5f5a7
utchak160 <chakravartyutkarsh@gmail.com>
2020-06-03 16:49:57 +0530

    backend separated

--- backend/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URL, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
